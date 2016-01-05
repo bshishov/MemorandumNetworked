cf5e6fb8841824edf5ec1b88a985d5163b639750
Boris Shishov <borisshishov@gmail.com>
2016-01-05 18:24:17 +0300

    Fixed folder form, relation automaticaly set by group key

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,9 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
   });
+
+
+var setRelation = function(relation) {
+    $( "input[name='relation']" ).val(relation);
+    $( "input[name='relation']" ).trigger('autoresize');
+};
