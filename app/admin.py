f96ff3850331f6dca911ce65dae176c26dc30a9c
Boris shishov <borisshishov@gmail.com>
2017-05-01 06:09:25 +0300

    Added profiles to the admin.

--- app/admin.py
+++ app/admin.py
@@ -1,18 +1,22 @@
 from django.contrib import admin
+import app.models as models
 
-from app.views import Node, Link, Url
 
 class NodeAdmin(admin.ModelAdmin):
-	list_display = ('id', 'user', 'text', 'date_added')
-	list_display_links = ('id', 'date_added')
+    list_display = ('id', 'user', 'text', 'date_added')
+    list_display_links = ('id', 'date_added')
+
 
 class LinkAdmin(admin.ModelAdmin):
-	list_display = ('user', 'node1', 'node2', 'provider1', 'provider2', 'relation')
+    list_display = ('user', 'node1', 'node2', 'provider1', 'provider2', 'relation')
+
 
 class UrlAdmin(admin.ModelAdmin):
-	list_display = ('user', 'url_hash', 'url', 'name', 'image', 'date_added')
-	list_display_links = ('url',)
+    list_display = ('user', 'url_hash', 'url', 'name', 'image', 'date_added')
+    list_display_links = ('url',)
 
-admin.site.register(Node, NodeAdmin)
-admin.site.register(Link, LinkAdmin)
-admin.site.register(Url, UrlAdmin)
+
+admin.site.register(models.Node, NodeAdmin)
+admin.site.register(models.Link, LinkAdmin)
+admin.site.register(models.Url, UrlAdmin)
+admin.site.register(models.Profile)
